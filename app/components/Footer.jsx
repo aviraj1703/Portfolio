3bc93b929673d2effd260f8d064a96d3a9bf9ba1
aviraj1703 <avirajrathod2002.ar@gmail.com>
2024-08-22 16:11:16 +0530

    Footer changed

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="footer w-full border-2 border-l-transparent border-r-transparent">
       <div className="container flex justify-end items-center p-8">
-        <p className="text-[#808080]">All rights reserved.</p>
+        <p className="text-[#808080]">No rights reserved &#128578;</p>
       </div>
     </footer>
   );
